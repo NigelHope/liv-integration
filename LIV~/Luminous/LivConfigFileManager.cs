2a2062ef8c06b951aa236ead83047501f34497eb
Douglas Hind <doug@luminousgroup.co.uk>
2020-05-05 10:20:30 +0100

    Added a config file manager Added existing code to namespace Adde a "read me"

---
+++ LIV~/Luminous/LivConfigFileManager.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using LIV.SDK.Unity;
+using UnityEngine;
+
+namespace Luminous.Aramco.Liv
+{
+    public class LivConfigFileManager : MonoBehaviour
+    {
+        private bool activeLastFrame = false;
+        private string realConfigFileName = "externalcamera.cfg";
+        private string configFileName = "LivConfig.cfg";
+
+        private void Awake()
+        {
+            if (File.Exists(realConfigFileName))
+            {
+                File.Delete(realConfigFileName);
+            }
+        }
+
+        private void Update()
+        {
+            bool active = SharedTextureProtocol.IsActive;
+            if (activeLastFrame != active)
+            {
+                if (active)
+                {
+                    byte[] readAllBytes = File.ReadAllBytes(configFileName);
+                    File.WriteAllBytes(realConfigFileName, readAllBytes);
+                }
+                else
+                {
+                    File.Delete(realConfigFileName);
+                }
+
+                activeLastFrame = active;
+            }
+        }
+    }
+}
